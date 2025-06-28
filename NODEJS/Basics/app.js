f706fba8f1d5b0bf69506fce6d77d794d703e02a
Oliver <softwaredevdad@gmail.com>
2025-06-28 12:06:51 +0200

    Changed CSS styling and added middleware for static files

--- NODEJS/Basics/app.js
+++ NODEJS/Basics/app.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
 // This will automatically consider our routes in the admin.js file.
 // When filing the request through the middlewares
 // Filtering our route via the /admin, so now the url has to go to /admin/add-product
@@ -20,9 +22,7 @@ app.use(shopRoutes);
 // Then because we are using the use route and no path as the first parameter
 // This will handle all http methods and not just ehg et or post
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "views", "404-page-not-found.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT);
